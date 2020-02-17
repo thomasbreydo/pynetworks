c2315c67e757b33ef67d7ba0a9ba95680a6ade16
Thomas Breydo <Thomas_Breydo@dalton.org>
2020-02-16 21:59:25 -0500

    bumping

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_dependencies():
 
 setup(
     name='pynetworks',
-    version='0.1',
+    version='0.1.0',
     author='Thomas Breydo',
     author_email='tbreydo@gmail.com',
     description='A Python implementation for networks of nodes',
