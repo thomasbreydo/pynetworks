10584f1f2fc40714b56853d35e705b2033609710
Thomas Breydo <Thomas_Breydo@dalton.org>
2020-02-16 22:08:50 -0500

    move docstring

--- pynetworks/__init__.py
+++ pynetworks/__init__.py
@@ -1,2 +1,9 @@
+'''
+pynetworks
+===============
+
+A package that provides structure for networks of interconnected nodes, using
+the DOT language for representation.'''
+
 __version__ = "0.1.1"
 from .core import Network, Node
--- pynetworks/core.py
+++ pynetworks/core.py
@@ -1,10 +1,3 @@
-'''
-pynetworks
-===============
-
-A package that provides structure for networks of interconnected nodes, using
-the DOT language for representation.'''
-
 from graphviz import Digraph
 
 
