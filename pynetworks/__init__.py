6ae0eea732950da7f550b0a04b6a958bd85d67d3
Thomas Breydo <Thomas_Breydo@dalton.org>
2020-03-03 11:06:12 -0500

    Connection format

--- pynetworks/__init__.py
+++ pynetworks/__init__.py
@@ -2,8 +2,9 @@
 pynetworks
 ===============
 
-A package that provides structure for networks of interconnected nodes, using
-the DOT language for representation.'''
+A package that provides structure for networks of interconnected nodes, using 
+the DOT language for representation.
+'''
 
 __version__ = "0.1.2"
 from .core import Network, Connection
