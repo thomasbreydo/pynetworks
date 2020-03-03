67d22e7e5200e5a5fc7cd6eb37b736ccb39fbb63
Thomas Breydo <Thomas_Breydo@dalton.org>
2020-03-03 11:17:52 -0500

    version

--- pynetworks/__init__.py
+++ pynetworks/__init__.py
@@ -6,5 +6,5 @@ A package that provides structure for networks of interconnected nodes, using
 the DOT language for representation.
 '''
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 from .core import Network, Connection
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_dependencies():
 
 setup(
     name='pynetworks',
-    version='0.2.0',
+    version='0.2.1',
     author='Thomas Breydo',
     author_email='tbreydo@gmail.com',
     description='A Python implementation for networks of nodes.',
