3c7cb1029d7354fe8284888ad2f165acd788768b
Thomas Breydo <tbreydo@gmail.com>
2020-05-15 23:16:50 -0400

    incorporate docs changes, prepare to switch to sphinx

--- setup.py
+++ setup.py
@@ -1,21 +1,6 @@
 from setuptools import setup, find_packages
 
-
-def get_long_description():
-    with open('docs/README.md') as readme:
-        return readme.read()
-
-
 setup(
-    name='pynetworks',
     version='0.4.13',
-    author='Thomas Breydo',
-    author_email='tbreydo@gmail.com',
-    description='A Python implementation for networks of nodes.',
-    long_description=get_long_description(),
-    long_description_content_type='text/markdown',
     packages=find_packages(),
-    license='GNU',
-    url='https://github.com/thomasbreydo/pynetworks',
-    include_package_data=True
 )
