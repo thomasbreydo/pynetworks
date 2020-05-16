8ae83afbd0b3ea8e72903e07110edbea82806da6
Thomas Breydo <tbreydo@gmail.com>
2020-05-15 23:46:42 -0400

    track version in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Thomas Breydo'
 author = 'Thomas Breydo'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.10'
+release = '0.4.14'
 
 
 # -- General configuration ---------------------------------------------------
