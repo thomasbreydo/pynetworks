ae0cb37d2680a48454ca7f8f67e89f9a22758dc2
Thomas Breydo <tbreydo@gmail.com>
2020-07-17 16:43:16 -0400

    fix version not importing

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
+import re
+
+with open("pynetworks/__init__.py", encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 setup(
     install_requires=[
         'pyperclip>=1.8.0',
-    ]
+    ],
+    version=version,
 )  # Metadata is in setup.cfg
